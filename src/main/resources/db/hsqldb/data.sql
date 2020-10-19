36fa2bf0d118db793ca0c822fab6cf972e22b535
alemandor1 <alemandor1@alum.us.es>
2020-10-19 18:10:43 +0200

    Dar de alta a alemandor1 como owner de una mascota. HECHO

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -42,7 +42,7 @@ INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
-INSERT INTO owners VALUES (20, 'Alejandro', 'Manzano', 'Rochelambert', 'Sevilla', '6062788369', 'owner1');
+INSERT INTO owners VALUES (20, 'Alejandro', 'Manzano', 'Rochelambert', 'Sevilla', '6062781369', 'owner1');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
